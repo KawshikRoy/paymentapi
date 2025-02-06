e347db4e74666b1f5f05b1c828dc93ee64e037d1
kawshik <kawshikaiub@gmail.com>
2025-02-06 12:24:44 +0600

    Revert "Remove Lombok"
    This reverts commit 04a2e4d152d3fc900bdd0d15bd63a9ce040c9acf.

--- src/main/java/com/itcbd/paymentapi/Entity/Customer.java
+++ src/main/java/com/itcbd/paymentapi/Entity/Customer.java
@@ -1,47 +1,17 @@
 package com.itcbd.paymentapi.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Customer {
     private String customerId;
     private String customerName;
     private String customerEmail;
     private String customerPhone;
-
-    public Customer(String customerId, String customerName, String customerEmail, String customerPhone) {
-        this.customerId = customerId;
-        this.customerName = customerName;
-        this.customerEmail = customerEmail;
-        this.customerPhone = customerPhone;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerEmail() {
-        return customerEmail;
-    }
-
-    public void setCustomerEmail(String customerEmail) {
-        this.customerEmail = customerEmail;
-    }
-
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
-
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
-    }
 }
